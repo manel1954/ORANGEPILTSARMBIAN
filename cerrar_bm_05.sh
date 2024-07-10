a6c315fa506f4be0312b838470ea10b0aa2d0d15
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-10 20:42:08 +0200

    1

--- cerrar_bm_05.sh
+++ cerrar_bm_05.sh
@@ -4,7 +4,7 @@ sudo killall MMDVMBM
 cd /home/orangepi/ORANGEPI
 sed -i "7c MMDVMBM=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=xterm -geometry 104x13+771+830 -bg brown -fg green -fa 'roboto' -fs 9x -T BRANDMEISTER -e sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
+sed -i "6c Exec=xterm -geometry 84x13+771+830 -bg brown -fg green -fa 'roboto' -fs 9x -T BRANDMEISTER -e sh ejecutar_bm_05.sh" /home/orangepi/Desktop/BM.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/DMR.png" /home/orangepi/Desktop/BM.desktop
 sed -i "10c Name[es_ES]=Abrir BM" /home/orangepi/Desktop/BM.desktop
 
