a59c7caca3119b5f2793648ffa707bb1bbab53e9
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-10 22:03:47 +0200

    1

--- ejecutar_solodstar.sh
+++ ejecutar_solodstar.sh
@@ -19,8 +19,9 @@ sudo ./MMDVMDSTAR MMDVMDSTAR.ini &
 cd /home/orangepi/ORANGEPI
 sh ejecutar_d-star_05.sh
 
-sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 --title=SOLODSTAR -x sh ejecutar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
+sed -i "6c Exec=xterm -geometry 85x13+1000+830 -bg brown -fg green -fa 'roboto' -fs 9x -T SOLODSTAR -e sh ejecutar_solodstar.sh" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/SOLO_D-STAR.png" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "11c Name[es_ES]=Abrir solo D-STAR" /home/orangepi/Desktop/AbrirsoloDstar.desktop
 sed -i "13c SOLODSTAR=OFF" /home/orangepi/status.ini
 
+ 
