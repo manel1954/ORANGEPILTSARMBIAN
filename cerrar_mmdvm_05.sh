651b73c30b265fe0256c192b5edab92c0ea7fa38
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-10 23:13:07 +0200

    1

--- cerrar_mmdvm_05.sh
+++ cerrar_mmdvm_05.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "6c Exec=mate-terminal --geometry 104x16+772+880 --title=RADIO -x sh ejecutar_mmdvm_05.sh" /home/orangepi/Desktop/MMDVM.desktop
+sed -i "6c Exec=xterm -geometry 85x13+655+830 -bg black -fg blue -fa 'roboto' -fs 9x -T RADIO -x sh ejecutar_mmdvm_05.sh" /home/orangepi/Desktop/MMDVM.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/MMDVM.png" /home/orangepi/Desktop/MMDVM.desktop
 sed -i "4c Name[es_ES]=Abrir Radio" /home/orangepi/Desktop/MMDVM.desktop
 
