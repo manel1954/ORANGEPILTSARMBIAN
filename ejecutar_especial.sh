d76712aa8d187bae5dfd6994fb478e43ddb357cf
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-10 23:07:14 +0200

    1

--- ejecutar_especial.sh
+++ ejecutar_especial.sh
@@ -26,6 +26,6 @@ sudo killall MMDVMESPECIAL
 cd /home/orangepi/ORANGEPI
 sed -i "10c MMDVMESPECIAL=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=mate-terminal --geometry 104x16+777+880 --title=ESPECIAL -x sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "6c Exec=xterm -geometry 85x13+655+830 -bg brown -fg green -fa 'roboto' -fs 9x -T ESPECIAL -e sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_OFF.png" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir  ESPECIAL" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
