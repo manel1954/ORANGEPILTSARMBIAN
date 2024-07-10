66c40c99e4efc44ae753e833543c7da1d135a0da
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-10 18:05:07 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash	                               
                                           
-                        cd /home/orangepi/ORANGEPI3
+                        cd /home/orangepi/ORANGEPILTSARMBIAN
                         
                         git pull --force
 
