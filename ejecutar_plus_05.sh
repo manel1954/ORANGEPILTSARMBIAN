0a38cdd7843b8c89450d5c9950a21c67ed64346d
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-11 11:13:14 +0200

    Update ejecutar_plus_05.sh

--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -6,18 +6,13 @@ sed -i "10c Name[es_ES]=Cerrar DMR+" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "6c MMDVMPLUS=ON" /home/orangepi/status.ini
 cd /home/orangepi/qt
 ./qt_callsign_log &
-echo "\33[1;37m"
+
 cd /home/orangepi/MMDVMHost
-echo "\33[1;32m"
-clear
-echo "***********************************************"
-echo "*                 DMR+                        * "
-echo "***********************************************"
-#sleep 2
-echo "\33[1;37m"
+
 udo killall MMDVMPLUS
-sudo ./MMDVMPLUS MMDVMPLUS.ini
- 
+
+xterm -geometry 86x13+24+830 -bg black -fg green -fa 'roboto' -fs 9x -T DMR+ -e sudo ./MMDVMPLUS MMDVMPLUS.ini
+
 sed -i "6c Exec=xterm -geometry 86x13+24+830 -bg black -fg green -fa 'roboto' -fs 9x -T DMR+ -e sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_DMRPLUS.png" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Abrir DMR+" /home/orangepi/Desktop/DMRPLUS.desktop
