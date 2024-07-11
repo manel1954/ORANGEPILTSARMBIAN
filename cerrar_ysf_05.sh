fd17a94d324da1f4f8a00584e30d253a2d67ba0c
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-11 10:51:42 +0200

    1

--- cerrar_ysf_05.sh
+++ cerrar_ysf_05.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 sudo killall YSFGateway
-sed -i "6c Exec=mate-terminal --geometry 104x16+1151+880 --title=YSF -x sh ejecutar_ysf_05.sh" /home/orangepi/Desktop/YSFGateway.desktop
+sed -i "6c Exec=xterm -geometry 86x13+1277+830 -bg brown -fg green -fa 'roboto' -fs 9x -T YSFGateway -e sh ejecutar_ysf_05.sh" /home/orangepi/Desktop/YSFGateway.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/C4FM.png" /home/orangepi/Desktop/YSFGateway.desktop
 sed -i "11c Name[es_ES]=Abrir YSF" /home/orangepi/Desktop/YSFGateway.desktop
 
 sed -i "3c YSF=OFF" /home/orangepi/status.ini
+
