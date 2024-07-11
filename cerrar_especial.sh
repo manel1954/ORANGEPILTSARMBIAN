5112814bf960c4ea87369d988dfdf56ac8bf3e92
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-11 12:32:12 +0200

    1

--- cerrar_especial.sh
+++ cerrar_especial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo killall MMDVMESPECIAL
 
-sed -i "6c Exec=xterm -geometry 85x13+655+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T ESPECIAL -e sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "6c Exec=xterm -geometry 86x13+1277+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T ESPECIAL -e sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_OFF.png" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir  ESPECIAL" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "10c MMDVMESPECIAL=OFF" /home/orangepi/status.ini
--- ejecutar_especial.sh
+++ ejecutar_especial.sh
@@ -14,12 +14,12 @@ sed -i "10c Name[es_ES]=Cerrar  ESPECIAL" /home/orangepi/Desktop/Abrir_ESPECIAL.
 sed -i "10c MMDVMESPECIAL=ON" /home/orangepi/status.ini
 
 cd /home/orangepi/MMDVMHost
-xterm -geometry 85x13+655+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T ESPECIAL -e sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
+xterm -geometry 86x13+1277+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T ESPECIAL -e sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
 
 sudo killall MMDVMESPECIAL
 cd /home/orangepi/ORANGEPI
 sed -i "10c MMDVMESPECIAL=OFF" /home/orangepi/status.ini
 
-sed -i "6c Exec=xterm -geometry 85x13+655+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T ESPECIAL -e sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
+sed -i "6c Exec=xterm -geometry 86x13+1277+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T ESPECIAL -e sh ejecutar_especial.sh" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_ESPECIAL_OFF.png" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir  ESPECIAL" /home/orangepi/Desktop/Abrir_ESPECIAL.desktop
