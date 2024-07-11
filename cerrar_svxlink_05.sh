46f51e65d0175c2363013425fb5c575b94b30f72
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-11 12:49:00 +0200

    Update cerrar_svxlink_05.sh

--- cerrar_svxlink_05.sh
+++ cerrar_svxlink_05.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo killall svxlink
-sed -i "6c Exec=mate-terminal --geometry 74x23+696+50 --title=SVXLINK -x sh ejecutar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
+sed -i "6c Exec=mate-terminal --geometry 68x27+22+50 --title=SVXLINK -x sh ejecutar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/ECHOLINK.png" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Abrir SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 
