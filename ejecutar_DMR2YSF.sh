327a916d303a83fb8e2a15c882e5fc1655786caa
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-12 11:22:13 +0200

    Update ejecutar_DMR2YSF.sh

--- ejecutar_DMR2YSF.sh
+++ ejecutar_DMR2YSF.sh
@@ -5,7 +5,7 @@ sed -i "7c Icon=/home/orangepi/ORANGEPI/DMR2YSF_ON.png" /home/orangepi/Desktop/A
 sed -i "10c Name[es_ES]=Cerrar DMR2YSF" /home/orangepi/Desktop/Abrir_DMR2YSF.desktop
 
 cd /home/orangepi/DMR2YSF
-xterm -geometry 86x8+1277+723 -bg black -fg yellow -fa 'roboto' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
+xterm -geometry 86x8+1277+680 -bg black -fg yellow -fa 'roboto' -fs 9x -T DMR2YSF -e ./DMR2YSF DMR2YSF.ini &
 
 cd /home/orangepi/MMDVMHost
 xterm -geometry 86x6+1277+830 -bg black -fg yellow -fa 'roboto' -fs 9x -T MMDVMDMR2YSF -e sudo ./MMDVMDMR2YSF MMDVMDMR2YSF.ini &
