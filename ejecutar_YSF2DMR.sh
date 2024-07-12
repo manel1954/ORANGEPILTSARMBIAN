6ceb196c9c12e487181e1610dc3e74fff49cb2b1
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-12 02:21:57 +0200

    Update ejecutar_YSF2DMR.sh

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -8,8 +8,8 @@ sed -i "10c Name[es_ES]=Cerrar YSF2DMR" /home/orangepi/Desktop/Abrir_YSF2DMR.des
 sed -i "14c YSF2DMR=ON" /home/orangepi/status.ini
 
 cd /home/orangepi/YSF2DMR
-xterm -geometry 86x6+1277+830 -bg brown -fg white -fa 'roboto' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini & 
+xterm -geometry 86x7+1277+803 -bg brown -fg white -fa 'roboto' -fs 9x -T YSF2DMR -e ./YSF2DMR YSF2DMR.ini & 
 
 cd /home/orangepi/MMDVMHost
-xterm -geometry 86x6+1277+987 -bg brown -fg white -fa 'roboto' -fs 9x -T MMDVMFUSION -e ./MMDVMFUSION MMDVMFUSION.ini
+xterm -geometry 86x7+1277+1014 -bg brown -fg white -fa 'roboto' -fs 9x -T MMDVMFUSION -e ./MMDVMFUSION MMDVMFUSION.ini
  
