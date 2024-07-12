eac63976c0cfc9c17ac9e48ec8d306c14f04009c
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-12 02:02:26 +0200

    1

--- ejecutar_plus_05.sh
+++ ejecutar_plus_05.sh
@@ -7,9 +7,9 @@ sed -i "6c MMDVMPLUS=ON" /home/orangepi/status.ini
 
 cd /home/orangepi/MMDVMHost
 
-xterm -geometry 86x13+24+830 -bg black -fg white -fa 'roboto' -fs 9x -T DMR+ -e sudo ./MMDVMPLUS MMDVMPLUS.ini
+xterm -geometry 86x16+24+803 -bg black -fg white -fa 'roboto' -fs 9x -T DMR+ -e sudo ./MMDVMPLUS MMDVMPLUS.ini
 
-sed -i "6c Exec=xterm -geometry 86x13+24+830 -bg black -fg white -fa 'roboto' -fs 9x -T DMR+ -e sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
+sed -i "6c Exec=xterm -geometry 86x16+24+803 -bg black -fg white -fa 'roboto' -fs 9x -T DMR+ -e sh ejecutar_plus_05.sh" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/ICONO_DMRPLUS.png" /home/orangepi/Desktop/DMRPLUS.desktop
 sed -i "10c Name[es_ES]=Abrir DMR+" /home/orangepi/Desktop/DMRPLUS.desktop
 
