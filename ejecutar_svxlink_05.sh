d684bd14555edd30e633cf6753232df8384b50a1
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-13 14:10:39 +0200

    Update ejecutar_svxlink_05.sh

--- ejecutar_svxlink_05.sh
+++ ejecutar_svxlink_05.sh
@@ -15,8 +15,8 @@ sed -i "4c Name[es_ES]=Cerrar SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desk
 sed -i "8c SVXLINK=ON" /home/orangepi/status.ini
 
 # sudo svxlink
-xterm -geometry 69x29+22+48 -bg black -fg grey -fa ‘verdana’ -fs 9x -T SVXLINK -e sudo svxlink
+xterm -geometry 69x29+22+48 -bg grey -fg black -fa ‘verdana’ -fs 9x -T SVXLINK -e sudo svxlink
 
-sed -i "6c Exec=xterm -geometry 69x29+22+48 -bg black -fg grey -fa ‘verdana’ -fs 9x -T SVXLINK -e sh ejecutar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
+sed -i "6c Exec=xterm -geometry 69x29+22+48 -bg grey -fg black -fa ‘verdana’ -fs 9x -T SVXLINK -e sh ejecutar_svxlink_05.sh" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "7c Icon=/home/orangepi/ORANGEPI/DMRPLUS_EDITOR.png" /home/orangepi/Desktop/Abrir_SVXLink.desktop
 sed -i "4c Name[es_ES]=Abrir SVXLINK" /home/orangepi/Desktop/Abrir_SVXLink.desktop
