7adb079053603791049a3270bcd86c212a22475b
manel1954 <55663831+manel1954@users.noreply.github.com>
2025-04-11 18:44:09 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -1,25 +1,13 @@
 #!/bin/bash	                               
                                           
-                        cd /home/orangepi/ORANGEPILTSARMBIAN
-                        
-                        git pull --force
-
-                        
-
+                        cd /home/orangepi/ORANGEPILTSARMBIAN                        
+                        git pull --force                        
                         sudo rm -R /home/orangepi/ORANGEPI
-
-                        mkdir /home/orangepi/ORANGEPI
-                                                
+                        mkdir /home/orangepi/ORANGEPI                                                
                         cp -R /home/orangepi/ORANGEPILTSARMBIAN/* /home/orangepi/ORANGEPI
-                        cp -R /home/orangepi/ORANGEPILTSARMBIAN/AUTOSTART/* /home/orangepi/AUTOSTART                      
-                        
-                        sudo rm -R /home/orangepi/qt
-
-                                      
+                        cp -R /home/orangepi/ORANGEPILTSARMBIAN/AUTOSTART/* /home/orangepi/AUTOSTART                                              
+                        sudo rm -R /home/orangepi/qt                                      
                         cp -R /home/orangepi/ORANGEPILTSARMBIAN/qt/ /home/orangepi/
-
-                        sudo chmod 777 -R /home/orangepi/ORANGEPI
-                        
+                        sudo chmod 777 -R /home/orangepi/ORANGEPI                    
                         sudo chmod 777 -R /home/orangepi/qt
-
                         sudo chmod 777 -R /home/orangepi/AUTOSTART
